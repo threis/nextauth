af9ab86b84a844939627607542a11b81347a2856
Thiago Reis <thiagoreis93@gmail.com>
2021-06-16 23:20:20 -0300

    First commit

---
+++ pages/dashboard.tsx
@@ -0,0 +1,19 @@
+import { parseCookies } from "nookies";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { api } from "../services/api";
+
+export default function Dashboard() {
+  const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    api
+      .get("/me")
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return <h1>Dashboard: {user?.email} </h1>;
+}
