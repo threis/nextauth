4f817523bf2a733e96b67c70d606aa65a6b4d36d
Thiago Reis <thiagoreis93@gmail.com>
2021-06-20 15:59:34 -0300

    add users permissions and roles

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -6,7 +6,7 @@ import { api } from "../services/apiClient";
 import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard() {
-  const { user } = useContext(AuthContext);
+  const { user, signOut } = useContext(AuthContext);
 
   useEffect(() => {
     api
@@ -20,6 +20,7 @@ export default function Dashboard() {
   return (
     <>
       <h1>Dashboard: {user?.email} </h1>
+      <button onClick={signOut}>Sign Out</button>
       <Can permissions={["metrics.list"]}>
         <div>Métricas</div>
       </Can>
